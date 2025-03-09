85e2a902e57266e34a817dc6c2c4ab0f8e9b05be
sisteminformatica <mmsi.sistemas@gmail.com>
2025-03-09 16:43:09 -0300

    dados da empresa no header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,7 +3,7 @@ import { StatsCard } from "../statcard/StatsCard";
 import styles from "./styles.module.scss";
 import { TasksContext } from "../../context/TasksContext";
 
-import logoimg from "../../assets/image/LogoSistem.jpeg";
+//import logoimg from "../../assets/image/LogoSistem.jpeg";
 
 export const Header: React.FC = () => {
     const { tasks } = useContext(TasksContext);
@@ -21,7 +21,6 @@ export const Header: React.FC = () => {
       <div className={styles.container}>
         <div>
           <h1>Minha Lista</h1>
-          <p><img src={logoimg} alt="" /></p>
           <p><a href="https://sisteminformaticacom.br"></a>Sistem Informática (85)99995-7239</p>          
         </div>
 
