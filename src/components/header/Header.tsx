e3c7d1b3631e845b5b54b242fa31c0b617f97d22
sisteminformatica <mmsi.sistemas@gmail.com>
2025-03-09 14:54:38 -0300

    olocado logo no header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,6 +3,8 @@ import { StatsCard } from "../statcard/StatsCard";
 import styles from "./styles.module.scss";
 import { TasksContext } from "../../context/TasksContext";
 
+import logoimg from "../../assets/image/LogoSistem.jpeg";
+
 export const Header: React.FC = () => {
     const { tasks } = useContext(TasksContext);
     const totalTasks = tasks.length;
@@ -19,7 +21,8 @@ export const Header: React.FC = () => {
       <div className={styles.container}>
         <div>
           <h1>Minha Lista</h1>
-          <p>Bem Vindo, Roberim</p>
+          <p><img src={logoimg} alt="" /></p>
+          <p><a href="https://sisteminformaticacom.br"></a>Sistem Informática (85)99995-7239</p>          
         </div>
 
         <div>
